42975436ac54116ac45873722afd6d51bf37312a
Orlando <orlandovasquezneira@gmail.com>
2021-05-13 22:47:03 -0400

    Creación del fichero imrich

---
+++ imrich/imrich/imrichApp.swift
@@ -0,0 +1,17 @@
+//
+//  imrichApp.swift
+//  imrich
+//
+//  Created by Orlando on 13-05-21.
+//
+
+import SwiftUI
+
+@main
+struct imrichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
